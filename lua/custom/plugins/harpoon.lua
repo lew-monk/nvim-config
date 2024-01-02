83c029aa7e4046fda549ed8d5d759e73f6741b94
lew-monk <lewmonk@gmail.com>
2024-01-02 12:12:57 +0300

    Harpoon Addition

---
+++ lua/custom/plugins/harpoon.lua
@@ -0,0 +1,15 @@
+return {
+	"ThePrimeagen/harpoon",
+	lazy = false,
+        branch = "harpoon2",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
+	config = true,
+	keys = {
+		{ "<leader>vm", "<cmd>lua require('harpoon.mark').add_file()<cr>", desc = "Mark file with harpoon" },
+		{ "<leader>vn", "<cmd>lua require('harpoon.ui').nav_next()<cr>", desc = "Go to next harpoon mark" },
+		{ "<leader>vp", "<cmd>lua require('harpoon.ui').nav_prev()<cr>", desc = "Go to previous harpoon mark" },
+		{ "<leader>va", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", desc = "Show harpoon marks" },
+	},
+}
